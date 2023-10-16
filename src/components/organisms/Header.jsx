0e7d581d2ad59f08be7c9620f50e7fc9c846e6bf
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-10-16 19:19:43 +0800

    test 1

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center h-screen" >
-      <div className="flex justify-center items-center ">
+    <header className="flex items-center justify-center h-screen ">
+      <div className="flex justify-center items-center flex-wrap gap-8">
         <Image
           src="/me.jpg"
           alt="Tharshen"
@@ -11,10 +11,10 @@ export default function Header() {
           height={200}
           className="rounded-[1000px]"
         />
-        <div className="ml-14" id="something" data-aos="fade-up">
-          <div className="py-8 w-[420px] px-10 rounded-lg drop-shadow-lg  bg-gradient-to-r from-gray-800 to-gray-900 animate-pulse">
-            <h1 className="font-[400] text-4xl">I&rsquo;m Tharshen</h1>
-            <h3 className="mt-1 font-[300]">Just a lifeless geek who loves programming</h3>
+        <div className="" id="something" data-aos="fade-up">
+          <div className="py-8 sm:w-[320px] lg:w-[420px] px-10 rounded-lg drop-shadow-lg  bg-gradient-to-r from-gray-800 to-gray-900 animate-pulse">
+            <h1 className="font-[400] sm:text-xl md:text-2xl lg:text-4xl sm:text-center lg:text-left" >I&rsquo;m Tharshen</h1>
+            <h3 className="mt-1 font-[300] ">Just a lifeless geek who loves programming</h3>
           </div>
         </div>
       </div>
