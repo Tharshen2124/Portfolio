821cb1fe0ef67e8a6fabe8785a63dca16c86423f
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-10-16 23:21:19 +0800

    feat: add repo links

--- src/components/organisms/Projects.jsx
+++ src/components/organisms/Projects.jsx
@@ -11,21 +11,26 @@ export default function Projects() {
             <div className="bg-yellow-500 text-black font-bold text-sm rounded-xl w-24 text-center py-1 my-2">In Progress</div>
                 <h1 className="font-bold">Restaurant Website</h1>
                 <p className="font-[300]">A website where a customer can add foods to their order and then checkout once they have confirmed their order items</p>
+                <Link href="https://github.com/Tharshen2124/Restaurant-frontend"><div className="transition text-blue-400 hover:text-blue-500 active:text-blue-600 underline mt-3">Frontend Repository Link </div></Link>
+                <Link href="https://github.com/Tharshen2124/Restaurant-api"><div className="transition text-blue-400 hover:text-blue-500 active:text-blue-600 underline mt-3">Backend Repository Link</div></Link>
             </div>
             <div className="w-[500px] py-5 px-10 rounded-xl bg-gradient-to-r from-gray-800 via-gray-900 to-black shadow-lg">
             <div className="bg-yellow-500 text-black font-bold text-sm rounded-xl w-24 text-center py-1 my-2">In Progress</div>
                 <h1 className="font-bold">RL Analyst</h1>
                 <p className="font-[300]">This website shows rocket league related statistics that is fetched from a Rocket League API</p>
+                <Link href="https://github.com/Tharshen2124/RL-Insights"><div className="transition text-blue-400 hover:text-blue-500 active:text-blue-600 underline mt-3">Repository Link</div></Link>
             </div>
             <div className="w-[500px] py-5 px-10 rounded-xl bg-gradient-to-r from-gray-800 via-gray-900 to-black shadow-lg">
             <div className="bg-yellow-500 text-black font-bold text-sm rounded-xl w-24 text-center py-1 my-2">In Progress</div>
                 <h1 className="font-bold">Quiz Web App</h1>
                 <p className="font-[300]">A simple quiz game i made that fetches data from the Open Trivia API. It will show after each question the correct answer and at the end of the quiz, it shows your score</p>
+                <Link href="https://github.com/Tharshen2124/quiz-app"><div className="transition text-blue-400 hover:text-blue-500 active:text-blue-600 underline mt-3">Repository Link</div></Link>
             </div>
             <div className="w-[500px] py-5 px-10 rounded-xl bg-gradient-to-r from-gray-800 via-gray-900 to-black shadow-lg">
             <div className="bg-green-500 text-black font-bold text-sm rounded-xl w-24 text-center py-1 my-2">Completed</div>
                 <h1 className="font-bold">File Sorter</h1>
                 <p className="font-[300]">A simple script i made that sorts the files you have in your files explorer and places them into their respective extension named folders</p>
+                <Link href="https://github.com/Tharshen2124/File-sorter"><div className="transition text-blue-400 hover:text-blue-500 active:text-blue-600 underline mt-3">Repository Link</div></Link>
             </div>
         </div>
         <h4 className="text-lg text-center mt-10 mb-5">See all my projects/repos on my Github!</h4>
