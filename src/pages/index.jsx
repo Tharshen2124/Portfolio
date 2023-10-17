ef2a512e9ac6ff33a47efee44429f372abed84d8
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-10-18 01:25:30 +0800

    feat: add a nav bar to laptop view

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,12 @@ import Achievements from "@/components/organisms/Achievements";
 import Footer from "@/components/organisms/Footer";
 import Header from "@/components/organisms/Header";
 import Projects from "@/components/organisms/Projects";
+import Navbar from "@/components/organisms/navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Header />
       <Achievements />
       <Projects/>
